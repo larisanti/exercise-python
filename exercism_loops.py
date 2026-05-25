22a92b407dbad9046ba6ae0ce64e6318f0b70624
Larissa de Santi <desantilarissa@gmail.com>
2026-05-25 11:04:40 -0300

    chore: rename loops -> exercism_loops

---
+++ exercism_loops.py
@@ -0,0 +1,135 @@
+"""
+Course:
+Python (Exercism)
+
+Objective:
+Develop functions for organizing and calculating student exam scores.
+
+Notes:
+- while loops -> indefinite = uncounted iteration
+- for loops -> definite = counted iteration
+- customize loop behavior: else, continue, break
+- counting and indexing: range(), enumerate()
+- range -> lazy objects = só consomem memória quando chamado (quase nada de espaço)
+^ pois não cria todos os nros de uma vez na memória
+- enumerate -> if both values and indexes are needed
+^ can be used as lookup if lengths and indexes line up
+- pop() method -> removes and returns the last item in a list
+"""
+
+
+def round_scores(student_scores):
+    """Round all provided student scores.
+
+    Parameters:
+        student_scores (list[float]): Student exam scores.
+
+    Returns:
+        list[int]: Student scores *rounded* to the nearest integer value.
+    """
+
+    rounded_scores = []
+
+    while student_scores:
+        score = student_scores.pop()
+        rounded_scores.append(round(score))
+
+    return rounded_scores
+
+
+def count_failed_students(student_scores):
+    """Count the number of failing students out of the group provided.
+
+    Parameters:
+        student_scores (list[int]): Student scores as ints.
+
+    Returns:
+        int: The count of student scores at or below 40.
+    """
+
+    count = 0
+
+    for score in student_scores:
+        if score <= 40:
+            count += 1
+
+    return count
+
+
+def above_threshold(student_scores, threshold):
+    """Determine how many of the provided student scores were 'the best' based on the provided threshold.
+
+    Parameters:
+        student_scores (list[int]): Integer scores.
+        threshold (int): The threshold to cross to be the "best" score.
+
+    Returns:
+        list[int]: Integer scores that are at or above the "best" threshold.
+    """
+
+    top_score = []
+
+    for score in student_scores:
+        if score >= threshold:
+            top_score.append(score)
+
+    return top_score
+
+
+def letter_grades(highest):
+    """Create a list of grade thresholds based on the provided highest grade.
+
+    Parameters:
+        highest (int): The value of the highest exam score.
+
+    Returns:
+        list[int]: Lower threshold scores for each D-A letter grade interval.
+
+        For example, where the highest score is 100, and failing is <= 40,
+        The result would be [41, 56, 71, 86]:
+            41 <= "D" <= 55
+            56 <= "C" <= 70
+            71 <= "B" <= 85
+            86 <= "A" <= 100
+    """
+
+    total_range = highest - 40
+    step = total_range // 4
+
+    return [41 + step * i for i in range(4)]
+
+
+def student_ranking(student_scores, student_names):
+    """Organize the student's rank, name, and grade information in descending order.
+
+    Parameters:
+        student_scores (list): Scores in descending order.
+        student_names (list[str]): Student names by exam score in descending order.
+
+    Returns:
+        list[str]: Strings in format ["<rank>. <student name>: <score>"].
+    """
+
+    ranking = []
+
+    for i in range(len(student_scores)):
+        ranking.append(f"{i + 1}. {student_names[i]}: {student_scores[i]}")
+
+    return ranking
+
+
+def perfect_score(student_info):
+    """Create a list that contains the name and grade of the first student to make a perfect score on the exam.
+
+    Parameters:
+        student_info (list[list[str, int]]): List of [<student name>, <score>] lists.
+
+    Returns:
+        list: First `[<student name>, 100]` found OR `[]` if no student score of 100 is found.
+    """
+
+    for student in student_info:
+        if student[1] == 100:
+            return student
+
+    return []
