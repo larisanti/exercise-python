4850c05514c328b30b2edcac14d93aeb2547a311
Larissa de Santi <desantilarissa@gmail.com>
2026-06-08 20:42:29 -0300

    algebra, function part 1

---
+++ algebra_function.py
@@ -0,0 +1,20 @@
+"""
+Curso:
+College Algebra with Python (freeCodeCamp)
+
+Objetivos:
+- Praticar notação de funções.
+- Compreender a lógica de input/output.
+
+Notes:
+- input/output = entra um valor e sai outro.
+
+OBS.: incompleto, preciso terminar de assistir à aula.
+"""
+
+# f(x) = x + 2 em python é:
+
+def f(x):
+    return x + 2
+
+print(f(3))
