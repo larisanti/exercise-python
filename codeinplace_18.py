5c8b2b78745bbfdfb4e415f9a5504b98c10e35f4
Larissa de Santi <desantilarissa@gmail.com>
2026-06-16 10:23:10 -0300

    codeinplace, exercise 18

---
+++ codeinplace_18.py
@@ -0,0 +1,19 @@
+"""
+Curso:
+Code in Place (Stanford)
+
+Objetivos:
+- Revisar história da linguagem python e hello world em arquitetura básica.
+
+Notes:
+- A "tradição" de começar com hello world foi popularizada pelo livro
+"The C Programming Language" (Dennis Ritchie).
+"""
+
+# Arquitetura começando com função main
+def main():
+    print("hello, world!")
+
+# Chama a função main -> standart boilerplate
+if __name__ == '__main__':
+    main()
