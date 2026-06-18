98e614a7e2ea8d9ecf1c4da31f69b1dafdd1cdf4
Larissa de Santi <desantilarissa@gmail.com>
2026-06-18 09:32:53 -0300

    codeinplace, exercise 20

---
+++ codeinplace_20.py
@@ -0,0 +1,26 @@
+"""
+Curso:
+Code in Place (Stanford)
+
+Objetivos:
+- Revisar f-strings e input.
+
+Notes:
+- Tenho achado bem básica a parte de tratamento de strings,
+mas estou fazendo outros exerícios no Exercism para complementar.
+"""
+
+"""
+Example of a Fill in the Blank program, like the one from the lesson.
+The user will enter three words (a color, an adjective and a goal).
+We will then turn them into a one sentence story.
+"""
+
+def main():
+    color = input("A color: ")
+    adjective = input("An adjective: ")
+    goal = input("A goal you would like to achieve: ")
+    print(f"At dawn the sky turned {color}, and the air felt {adjective}. I decided today I will finally {goal}.")
+
+if __name__ == "__main__":
+    main()
