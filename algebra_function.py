43e29d227b57d8e0d0f1f36517fde8874940cff9
Larissa de Santi <desantilarissa@gmail.com>
2026-06-22 21:41:54 -0300

    feat: algebra, function part 2

--- algebra_function.py
+++ algebra_function.py
@@ -4,12 +4,13 @@ College Algebra with Python (freeCodeCamp)
 
 Objetivos:
 - Praticar notação de funções.
-- Compreender a lógica de input/output.
+- Compreender a lógica de input/output em funções.
 
 Notes:
-- input/output = entra um valor e sai outro.
-
-OBS.: incompleto, preciso terminar de assistir à aula.
+- função consiste em:
+    1. receber um input, 
+    2. realizar uma operação com ela,
+    3. gerar um output
 """
 
 # f(x) = x + 2 em python é:
@@ -18,3 +19,12 @@ def f(x):
     return x + 2
 
 print(f(3))
+
+# automatizar a função com loop
+def f(x):
+    y = 10 * x + 5
+    return y 
+
+for x in range(5):
+    resultado = f(x)
+    print(x, "\t", resultado)
