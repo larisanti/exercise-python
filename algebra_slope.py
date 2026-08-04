bff485504a6e24f9af9e1025c987771572c8301a
Larissa de Santi <desantilarissa@gmail.com>
2026-08-03 23:00:10 -0300

    feat: algebra, exercise 10

---
+++ algebra_slope.py
@@ -0,0 +1,21 @@
+"""
+Curso: 
+College Algebra with Python (freeCodeCamp)
+
+Objetivo: 
+Calcular a inclinação (slope) de uma reta.
+
+Notes:
+- slope = M -> how much we move on the graph
+- lembrar dos parenteses:
+  M = (y2 - y1) / (x2 - x1)
+"""
+
+x1 = 1
+y1 = 5
+x2 = 2
+y2 = 7
+
+slope = (y2 - y1) / (x2 - x1)
+
+print("slope = ", slope)
