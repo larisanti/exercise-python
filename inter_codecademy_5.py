db8e2226c2b13bfbad539a679b68315180bf7b3d
Larissa de Santi <desantilarissa@gmail.com>
2026-08-12 11:31:24 -0300

    add inter, exercise 5

---
+++ inter_codecademy_5.py
@@ -0,0 +1,39 @@
+"""
+Curso:
+Learn Intermediate Python 3 (Codecademy)
+
+Objetivo:
+Praticar 
+
+Notes:
+- **kwargs = keyword arguments
+^ pode usar outro nome
+- gera o tipo dict -> pares chave=valor (nome='Larissa')
+"""
+
+tables = {
+  1: {
+    'name': 'Chioma',
+    'vip_status': False,
+    'order': {
+      'drinks': 'Orange Juice, Apple Juice',
+      'food_items': 'Pancakes'
+    }
+  },
+  2: {},
+  3: {},
+  4: {},
+  5: {},
+  6: {},
+  7: {},
+}
+print(tables)
+
+def assign_food_items(**order_items):
+    print(order_items)
+    food = order_items.get('food')
+    drinks = order_items.get('drinks')
+    print(food)
+    print(drinks)
+
+assign_food_items(food='Pancakes, Poached Egg', drinks='Water')
