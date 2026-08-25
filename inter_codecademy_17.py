7c22e04f2fcb8a7be49bcf138ac24e2d76202968
Larissa de Santi <desantilarissa@gmail.com>
2026-08-25 10:10:23 -0300

    add inter, exercise 17

---
+++ inter_codecademy_17.py
@@ -0,0 +1,35 @@
+"""
+Curso:
+Learn Intermediate Python 3 (Codecademy)
+
+Objetivo:
+Praticar modificação de escopo (nonlocal statement).
+
+Notes:
+- nonlocal statement = permite modificar nome de variável de escopo externo
+- UnboundLocalError = quando tento modificar variável dentro de nested function
+^ usar "nonlocal" antes do nome da variável pra modificar
+"""
+
+walls = [(20, 9), (25, 9), (20, 9), (25, 9)]
+
+def calc_paint_amount(wall_measurements):
+
+  square_feet = 0
+
+  def calc_square_feet():
+    
+    nonlocal square_feet
+    for width, height in wall_measurements:
+      square_feet += width * height
+
+  def calc_gallons():
+    return square_feet / 400
+
+  calc_square_feet()
+
+  return calc_gallons()
+
+
+print('Number of paint gallons needed: ')
+print(str(calc_paint_amount(walls)))
